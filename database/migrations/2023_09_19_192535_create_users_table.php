7a79965188850eedd546b580a3f31f36242e9695
PanMusztard <kuba.swinarski@o2.pl>
2023-09-19 22:02:46 +0200

    added database users and seeder to this

--- database/migrations/2023_09_19_192535_create_users_table.php
+++ database/migrations/2023_09_19_192535_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -13,11 +14,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->double('is_verified')->default(false);
+            $table->string('username')->unique();
+            $table->string('password', 72);
+            $table->boolean('is_admin')->default(false);
+            $table->string('token')->default(Str::random(10));
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 };
