cc2f5d9fb38f45e95d3fc462897ea46043e02b65
PanMusztard <kuba.swinarski@o2.pl>
2023-09-20 20:50:53 +0200

    not fully added email functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetTokenIsGoodRequest;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
@@ -17,14 +19,16 @@ use Illuminate\Validation\ValidationException;
  */
 class AuthController extends Controller
 {
-    public function getUser(){
+    public function getUser()
+    {
         auth()->id();
     }
 
     /**
      * @throws ValidationException
      */
-    public function login(LoginRequest $request){
+    public function login(LoginRequest $request)
+    {
         $data = $request->validated();
 
         $user = $this->userRepository->findByUsername($data['username']);
@@ -45,7 +49,9 @@ class AuthController extends Controller
         ];
 
     }
-    public function register(RegisterRequest $request){
+
+    public function register(RegisterRequest $request)
+    {
         $data = $request->validated();
         $user = new User();
         $user->username = $data['username'];
@@ -57,6 +63,9 @@ class AuthController extends Controller
 
         $user->save();
         event(new Registered($user));
+        $user->sendEmailVerificationNotification();
         return $user;
     }
+
+
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
         return [
             'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'is_verified' => fake()->boolean(),
+            'email_verified_at' => fake()->time,
             'is_admin' => fake()->boolean(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'token' => Str::random(10),
