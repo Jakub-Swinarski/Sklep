2500a397937feaaddffbefd5dbe5a8861c69dd33
PanMusztard <kuba.swinarski@o2.pl>
2023-09-23 22:59:57 +0200

    reset password by email done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DeleteUserRequest;
+use App\Http\Requests\EmailRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use App\Models\User;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
@@ -19,7 +24,7 @@ class AuthController extends Controller
 {
     public function getUser()
     {
-        auth()->id();
+        return auth()->id();
     }
 
     /**
@@ -83,6 +88,33 @@ class AuthController extends Controller
             ->where('id', '=' , auth()->id())
             ->delete();
     }
+    public function emailPasswordReset (EmailRequest $request){
+        $request->validated();
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
 
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+    public function resetPassword (ResetPasswordRequest $request){
+        $request->validated();
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
 
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register',[\App\Http\Controllers\AuthController::class, 'register']);
-Route::get('/getUser', [\App\Http\Controllers\AuthController::class, 'getUser'])->middleware('auth');
-Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+Route::get('/getUser', [\App\Http\Controllers\AuthController::class, 'getUser'])
+    ->middleware('auth');
 Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->name('verification.notice');
+    return view('auth.verify-email');}) // do to fronta
+    ->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 })->middleware('signed','auth')->name('verification.verify');
+
+Route::post('/forgot-password', [\App\Http\Controllers\AuthController::class, 'emailPasswordReset'])
+    ->name('password.email');
+Route::post('/reset-password',[\App\Http\Controllers\AuthController::class, 'resetPassword']);
+Route::get('/logout',[\App\Http\Controllers\AuthController::class, 'logout'])
+    ->middleware('auth');
