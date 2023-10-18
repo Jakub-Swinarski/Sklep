9206b6eb1b6947ac1d815638efe9bd317022dc06
PanMusztard <kuba.swinarski@o2.pl>
2023-10-18 20:46:35 +0200

    added size migration and orders migrations

---
+++ database/migrations/2023_10_18_110036_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->string('nameSurname');
+            $table->string('address');
+            $table->integer('zipcode');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2023_10_18_120200_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('type_of_delivery')
+                ->references('id')
+                ->on('type_of_delivery')
+                ->onDelete('cascade');
+            $table->foreignId('address')
+                ->references('id')
+                ->on('address')
+                ->onDelete('cascade');
+            $table->boolean('pay_online');
+            $table->foreignId('invoice_id')
+                ->references('id')
+                ->on('invoices')
+                ->onDelete('cascade');
+            $table->integer('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
