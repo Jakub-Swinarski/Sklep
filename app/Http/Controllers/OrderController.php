357b20cfc44f3bc49540632c302c8cc9b542a3cd
PanMusztard <kuba.swinarski@o2.pl>
2023-10-20 12:02:29 +0200

    order controller and crud

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DeleteOrderRequest;
+use App\Http\Requests\EditOrderRequest;
+use App\Http\Requests\GetOrderRequest;
+use App\Http\Requests\GetUserOrdersRequest;
+use App\Http\Requests\NewOrderRequest;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function newOrder(NewOrderRequest $request){
+
+    }
+    public function getUserOrders(GetUserOrdersRequest $request){
+
+    }
+    public function getOrder(GetOrderRequest $request){
+
+    }
+    public function editOrder(EditOrderRequest $request){
+
+    }
+    public function deleteOrder(DeleteOrderRequest $request){
+
+    }
+}
